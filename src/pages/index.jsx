ac241300e9a2e921082811dfb4c8365b8c7ffce2
benmizes314 <50121405+benmizes314@users.noreply.github.com>
2019-04-29 17:18:23 -0500

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -105,23 +105,20 @@ const Index = () => (
         <AboutHero>
           <Avatar src={avatar} alt="Ben Mizes" />
           <AboutSub>
-            The English language can not fully capture the depth and complexity of my thoughts. So I'm incorporating
-            Emoji into my speech to better express myself. Winky face.
+            Do or do not. There is no try. -
           </AboutSub>
         </AboutHero>
         <AboutDesc>
-          You know the way you feel when you see a picture of two otters holding hands? That's how you're gonna feel
-          every day. My mother cried the day I was born because she knew she’d never be prettier than me. You should
-          make me your campaign manager. I was born for politics. I have great hair and I love lying. Captain? The kids
-          want to know where Paulie the Pigeon is. I told them he got sucked up into an airplane engine, is that all
-          right?
+       I'm Ben. I'm a real estate investor that focuses on Multifamily real estate in St. Louis, and the Founder of Clever Real Estate.
+          Clever let's anyone list their home for a flat fee of $3,000, or 1% if their home is more expensive.
+          In my free time I like St. Louis Blues Hockey, Camping, and my dog. 
         </AboutDesc>
       </About>
       <Contact offset={4}>
         <Inner>
           <Title>Get in touch</Title>
           <ContactText>
-            Say <a href="mailto:plizNoSp4m@domain.tld">Hi</a> or find me on other platforms:{' '}
+            Say <a href="mailto:ben@movewithclever.com">Hi</a> or find me on other platforms:{' '}
             <a href="https://medium.com/@benmizes">Medium</a> &{' '}
             <a href="https://www.listwithclever.com/">Clever</a>
           </ContactText>
