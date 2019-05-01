5ce7ef68630d7b567f1aa1ce5037a04d2847d8cb
benmizes314 <50121405+benmizes314@users.noreply.github.com>
2019-04-30 21:32:11 -0500

    Update website.js

--- config/website.js
+++ config/website.js
@@ -3,14 +3,14 @@ const tailwind = require('../tailwind')
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Ben Mizes', // Navigation and Site Title
+  siteTitle: 'Ben Mizes: Official Site', // Navigation and Site Title
   siteTitleAlt: 'Ben Mizes', // Alternative Site title for SEO
   siteTitleShort: 'Ben', // short_name for manifest
   siteHeadline: 'Ben Mizes: Entreprenuer, Real Estate Investor, and Dog Dad', // Headline for schema.org JSONLD
   siteUrl: 'https://www.benmizes.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logo.png', // Used for SEO and manifest
-  siteDescription: 'Ben Mizes - Real Estate Investor and Entrepreneur',
+  siteDescription: 'Ben Mizes - Real Estate Investor, Entrepreneur, and proud St. Louis native ',
   author: 'LekoArts', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
